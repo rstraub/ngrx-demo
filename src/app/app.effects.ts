5cc6bd3bf913d17e0045542526f35ec307570c70
Roy Straub <roy.straub@codesquad.nl>
2018-05-13 14:10:47 +0200

    Patch type checking

--- src/app/app.effects.ts
+++ src/app/app.effects.ts
@@ -4,7 +4,7 @@ import {TodoService} from './todo.service';
 import {Observable} from 'rxjs/Observable';
 import {catchError, map, mergeMap} from 'rxjs/operators';
 import {Action} from '@ngrx/store';
-import {AppActionTypes, TodosLoaded} from './app.actions';
+import {AddTodo, AppActionTypes, GetTodos, TodosLoaded, UpdateTodo} from './app.actions';
 
 
 @Injectable()
@@ -13,39 +13,39 @@ export class AppEffects {
 
   @Effect()
   getTodos$: Observable<Action> = this.actions$.pipe(
-    ofType(AppActionTypes.GET_TODOS),
+    ofType<GetTodos>(AppActionTypes.GET_TODOS),
     mergeMap(() =>
       this.todoService.getTodos().pipe(
         // If successful, dispatch success action with result
         map(todos => new TodosLoaded(todos)),
         // If request fails, dispatch failed action
-        catchError(() => console.error('handle the error here'))
+        // catchError(() => console.error('handle the error here'))
       )
     )
   );
 
   @Effect()
   addTodo$: Observable<Action> = this.actions$.pipe(
-    ofType(AppActionTypes.ADD_TODO),
+    ofType<AddTodo>(AppActionTypes.ADD_TODO),
     mergeMap(action =>
       this.todoService.addTodo(action.payload).pipe(
         // If successful, dispatch success action with result
         map(todos => new TodosLoaded(todos)),
         // If request fails, dispatch failed action
-        catchError(() => console.error('handle the error here'))
+        // catchError(() => console.error('handle the error here'))
       )
     )
   );
 
   @Effect()
   updateTodo$: Observable<Action> = this.actions$.pipe(
-    ofType(AppActionTypes.UPDATE_TODO),
+    ofType<UpdateTodo>(AppActionTypes.UPDATE_TODO),
     mergeMap(action =>
       this.todoService.updateTodo(action.payload).pipe(
         // If successful, dispatch success action with result
         map(todos => new TodosLoaded(todos)),
         // If request fails, dispatch failed action
-        catchError(() => console.error('handle the error here'))
+        // catchError(() => console.error('handle the error here'))
       )
     )
   );
